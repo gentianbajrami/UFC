5618cab6d0f806d30fa3d9118e454795c6c11c84
Gentian <gentian.bajrami20@outlook.com>
2025-05-14 17:03:12 +0200

    added athleted reducers, some configurations in package.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import AppProvider from './context/AppContext.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AthleteProvider from './context/AthleteContext.jsx';
+import AthleteProvider from './context/AthletesContext.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
